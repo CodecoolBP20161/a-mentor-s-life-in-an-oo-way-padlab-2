5f3dffa17b751c62d23a293d2cb0151f0924281a
vogronicspatrik <vogronicspatrik@gmail.com>
2016-06-22 13:10:22 +0200

    student and person class is done without inheritance

--- student.py
+++ student.py
@@ -2,4 +2,36 @@ from person import Person
 
 
 class Student(Person):
-    pass
+
+    def __init__(self, depend_level, knowledge_level, is_bet_on_expected_result, table_football_level, energy_level):
+
+        self.depend_level = depend_level
+        self.knowledge_level = knowledge_level
+        self.is_bet_on_expected_result = is_bet_on_expected_result
+        self.table_football_level = table_football_level
+        self.energy_level = energy_level
+
+    def bet(self, match):
+        if match.important = False:
+            if self.depend_level == "never" or self.depend_level == "only_interesting":
+                if self.is_bet_on_expected_result == was_it_expected:
+                    self.energy_level -= 50
+                else:
+                    self.energy_level += 20
+            else:
+                if self.is_bet_on_expected_result == was_it_expected:
+                    self.energy_level += 50
+                else:
+                    self.energy_level -= 100
+
+        if match.important = True:
+            if self.depend_level == "never":
+                if self.is_bet_on_expected_result == was_it_expected:
+                    self.energy_level -= 50
+                else:
+                    self.energy_level += 20
+            else:
+                if self.is_bet_on_expected_result == was_it_expected:
+                    self.energy_level += 50
+                else:
+                    self.energy_level -= 100
