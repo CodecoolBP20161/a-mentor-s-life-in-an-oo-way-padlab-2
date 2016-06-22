a44a691b303a9d70f91ef0ce5eecf4e08f721baf
vogronicspatrik <vogronicspatrik@gmail.com>
2016-06-22 13:38:53 +0200

    student with inheritance

--- student.py
+++ student.py
@@ -3,8 +3,9 @@ from person import Person
 
 class Student(Person):
 
-    def __init__(self, depend_level, knowledge_level, is_bet_on_expected_result, table_football_level, energy_level):
+    def __init__(self, first_name, last_name, year, gender, depend_level, knowledge_level, is_bet_on_expected_result, table_football_level, energy_level):
 
+        super().__init__(first_name, last_name, year, gender)
         self.depend_level = depend_level
         self.knowledge_level = knowledge_level
         self.is_bet_on_expected_result = is_bet_on_expected_result
