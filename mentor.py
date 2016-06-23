dc2f080720f5858d53ed2ecd400eba73c08405d1
Leviathan11 <gubik.levente@gmail.com>
2016-06-23 11:00:32 +0200

    mentor class can generate list of mentors

--- mentor.py
+++ mentor.py
@@ -21,3 +21,15 @@ class Mentor(Person):
     def feedback(cls, class):
         for student in class.students:
             student.energy_level += 15
+
+    def generate_list(cls, file_name):
+
+        list_of_mentors = []
+
+        with open('data/' + file_name) as csvfile:
+            all_mentors = csv.reader(csvfile)
+            for mentor in all_mentors:
+                mentor_object = Mentor(mentor[0], mentor[1], mentor[2], mentor[3], mentor[4], mentor[5])
+                list_of_mentors.append(mentor_object)
+
+        return list_of_mentors
